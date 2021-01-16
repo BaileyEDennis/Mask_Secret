fb34d8a116504790c5bb3ebd7be4740e2570a623
Bailey <bailey.elise.dennis@gmail.com>
2021-01-16 09:55:28 -0600

    added program for mask sensitive information

---
+++ MaskInfo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MaskInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Secret Prhase:");
+            string line = Console.ReadLine();
+            string hidden = "";
+
+            for (int i = 0; i < line.Length - 4; i++)
+            {
+                hidden += "[]";
+            }
+
+            string reveal = line.Substring(line.Length - 4);
+            Console.WriteLine($"Revealed: {hidden}{reveal}");
+
+        }
+    }
+}
